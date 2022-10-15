0d0971476567621a5b0ae1474346b6d8a6db630d
FaraJM21 <farruxrihsiyev@gmail.com>
2022-10-15 08:53:59 +0500

    home deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home, Problem, Login, Register,Subject, Main} from "./pages";
+import { Problem, Login, Register, Subject, Main } from "./pages";
 
 const App: FC = () => (
   <div>
     <Routes>
-      
-      <Route path='' element={<Home />}>
-      <Route index element={<Login />} />
+      <Route path="" element={<Main />}>
+        <Route index element={<Subject />} />
+        <Route path='problem' element={<Problem />} />
+      </Route>
+      <Route path="login" element={<Login />} />
       <Route path='register' element={<Register />} />
-      <Route path="main" element = {<Main/>}>
-      <Route index element={<Subject />} />
-      <Route path='problem' element={<Problem />} />
-      </Route>
-      
-      </Route>
-      
-   
+
     </Routes>
   </div>
 );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as Home} from "./home/Home";
 export { default as Problem } from "./problem/Problem";
 export { default as Login } from "./login/Login";
 export { default as Register } from "./register/Register";
